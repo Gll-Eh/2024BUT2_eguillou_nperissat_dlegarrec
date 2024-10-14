0df983e34f991b663d552e463242389dac276b85
nathanp13 <nath.peri04@gmail.com>
2024-10-14 14:16:40 +0200

    fin

--- app.js
+++ app.js
@@ -1,9 +1,29 @@
 const express= require('express');
 const app = express();
+const UserModel = require("./models/user.js");
 
 app.set('view engine', 'ejs');
 
 app.use(express.static('public'));
+
+
+
+//EXEMPLE A UTILISER ET A MODIFIER
+
+app.get('/', async function (req, res) {
+    try {
+        const user = await UserModel.getUserById(2);
+        res.render('index', {user});
+        console.log(user)
+    } catch (err) {
+        console.log(err);
+        res.status(500).send('Erreur');
+    }
+});
+
+//FIN DE LEXEMPLE
+
+
 
 app.get('/', function(req, res) {
     let data = {
@@ -20,3 +40,5 @@ app.use( function (req, res) {
 app.listen(3000, function () {
     console.log('Server running on port 3000');
 });
+
+
